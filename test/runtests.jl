4f5852aa641a92af893411c487fff089c1ab46c1
ZenanH <zenan.huo@outlook.com>
2025-06-18 23:37:52 +0200

    🍽 Add test for `rasterize` module

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,8 +3,9 @@ using CondaPkg
 using PythonCall
 using Test
 
-@test !PythonCall.pyisnull(FastPointQuery.np      )
-@test !PythonCall.pyisnull(FastPointQuery.shapely )
-@test !PythonCall.pyisnull(FastPointQuery.o3d     )
-@test !PythonCall.pyisnull(FastPointQuery.trimesh )
-@test !PythonCall.pyisnull(FastPointQuery.rasterio)
+@test !PythonCall.pyisnull(FastPointQuery.np       )
+@test !PythonCall.pyisnull(FastPointQuery.shapely  )
+@test !PythonCall.pyisnull(FastPointQuery.o3d      )
+@test !PythonCall.pyisnull(FastPointQuery.trimesh  )
+@test !PythonCall.pyisnull(FastPointQuery.rasterio )
+@test !PythonCall.pyisnull(FastPointQuery.rasterize)
