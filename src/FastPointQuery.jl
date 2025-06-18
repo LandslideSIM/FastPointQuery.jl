a0ef8024dfb9309de2f76d313ef1668127e91213
ZenanH <zenan.huo@outlook.com>
2025-06-18 23:36:46 +0200

    🔧 Update `QueryPolygon` struct and function

--- src/FastPointQuery.jl
+++ src/FastPointQuery.jl
@@ -2,12 +2,18 @@ module FastPointQuery
 
 using CondaPkg, PythonCall
 
+# Python packages
 const np       = PythonCall.pynew()
 const shapely  = PythonCall.pynew()
 const o3d      = PythonCall.pynew()
 const trimesh  = PythonCall.pynew()
 const rasterio = PythonCall.pynew()
+const pyjson   = PythonCall.pynew()
 
+# Python subpackages
+const rasterize = PythonCall.pynew()
+
+# PythonCall functions
 const py2ju = PythonCall.pyconvert
 const pyfun = PythonCall.pybuiltins
 
@@ -19,6 +25,9 @@ function __init__()
         PythonCall.pycopy!(o3d     , PythonCall.pyimport("open3d"  ))
         PythonCall.pycopy!(trimesh , PythonCall.pyimport("trimesh" ))
         PythonCall.pycopy!(rasterio, PythonCall.pyimport("rasterio"))
+        PythonCall.pycopy!(pyjson  , PythonCall.pyimport("json"    ))
+        # import submodules
+        PythonCall.pycopy!(rasterize, pyimport("rasterio.features").rasterize)
     catch e
         @error "Failed to initialize Python ENV" exception=e
     end
@@ -44,6 +53,22 @@ end
 
 struct QueryPolygon
     polygon::Py
+    coord  ::AbstractVector
+end
+
+function QueryPolygon(pypoly::Py)
+    @pyexec """
+    def py_tmp(poly, np):
+        rings = []
+        exterior = np.array(poly.exterior.coords)
+        rings.append(exterior)
+        for interior in poly.interiors:
+            hole = np.array(interior.coords)
+            rings.append(hole)
+        return rings
+    """ => py_tmp
+    coord = py2ju(Vector, py_tmp(pypoly, np))
+    return QueryPolygon(pypoly, coord)
 end
 
 include(joinpath(@__DIR__, "utils.jl"))
@@ -52,7 +77,5 @@ include(joinpath(@__DIR__, "polyhedron.jl"))
 
 # export structs
 export QueryPolygon, STLInfo2D, STLInfo3D
-# export functions
-export get_polygon, pip_query, readSTL2D, readSTL3D
 
 end
