a610d537e102310f0b9fbe6484832161980f4f1b
ZenanH <zenan.huo@outlook.com>
2025-12-13 18:53:20 +0100

    Initialize current version

--- src/FastPointQuery.jl
+++ src/FastPointQuery.jl
@@ -8,7 +8,6 @@ export pypkg_versions
 const np         = PythonCall.pynew()
 const shapely    = PythonCall.pynew()
 const o3d        = PythonCall.pynew()
-const trimesh    = PythonCall.pynew()
 const rasterio   = PythonCall.pynew()
 const pyjson     = PythonCall.pynew()
 const splashsurf = PythonCall.pynew()
@@ -29,13 +28,10 @@ function __init__()
         PythonCall.pycopy!(np        , PythonCall.pyimport("numpy"       ))
         PythonCall.pycopy!(shapely   , PythonCall.pyimport("shapely"     ))
         PythonCall.pycopy!(o3d       , PythonCall.pyimport("open3d"      ))
-        PythonCall.pycopy!(trimesh   , PythonCall.pyimport("trimesh"     ))
         PythonCall.pycopy!(rasterio  , PythonCall.pyimport("rasterio"    ))
         PythonCall.pycopy!(pyjson    , PythonCall.pyimport("json"        ))
         PythonCall.pycopy!(splashsurf, PythonCall.pyimport("pysplashsurf"))
         PythonCall.pycopy!(meshio    , PythonCall.pyimport("meshio"      ))
-        # import submodules
-        PythonCall.pycopy!(rasterize, pyimport("rasterio.features").rasterize)
     catch e
         @error "Failed to initialize Python ENV" exception=e
     end
@@ -46,23 +42,12 @@ function pypkg_versions()
     println("numpy:        ", np.__version__)
     println("shapely:      ", shapely.__version__)
     println("open3d:       ", o3d.__version__)
-    println("trimesh:      ", trimesh.__version__)
     println("rasterio:     ", rasterio.__version__)
     println("splashsurf:   ", pyhasattr(splashsurf, "__version__") ? splashsurf.__version__ : "N/A")
     println("meshio:       ", meshio.__version__)
     println("================================\n")
 end
 
-include(joinpath(@__DIR__, "fileio/asc.jl"))
-include(joinpath(@__DIR__, "fileio/geojson.jl"))
-include(joinpath(@__DIR__, "fileio/ply.jl"))
-include(joinpath(@__DIR__, "fileio/stl.jl"))
-include(joinpath(@__DIR__, "fileio/tiff.jl"))
-include(joinpath(@__DIR__, "fileio/xyz.jl"))
-
-include(joinpath(@__DIR__, "utils.jl"))
-include(joinpath(@__DIR__, "polygon.jl"))
-include(joinpath(@__DIR__, "polyhedron.jl"))
 
 export get_resource, res_dir
 
@@ -80,15 +65,5 @@ function get_resource()
              wheel          = joinpath(res_dir, "wheel.stl"))
     return model
 end
-
-quiet(f) = redirect_stdout(devnull) do
-    redirect_stderr(devnull) do
-        with_logger(NullLogger()) do
-            f()
-        end
-    end
-end
-
-#include(joinpath(@__DIR__, "precompile.jl"))
 
 end
